f34629349bb28ebabe4a6590af79ea9374e020c3
Anna <annaersh1994@gmail.com>
2021-07-15 22:01:27 +0300

    Initial Commit

---
+++ RSSchool_T8/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  RSSchool_T8
+//
+//  Created by Anna Ershova on 15.07.2021.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ RSSchool_T8/ViewController.m
@@ -0,0 +1,22 @@
+//
+//  ViewController.m
+//  RSSchool_T8
+//
+//  Created by Anna Ershova on 15.07.2021.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
